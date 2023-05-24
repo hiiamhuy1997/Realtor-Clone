12f2f46fe356d4094f7d98a4bb853e78095c6581
Quang Huy <hiiamhuy160997@gmail.com>
2023-05-24 12:45:10 +0700

    created pages and routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+import ForgotPassword from "./pages/ForgotPassword";
+import Offer from "./pages/Offer";
+
 function App() {
   return (
-    <>
-      <h1 className="">Hello World</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/sign-up" element={<Signup />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/offer" element={<Offer />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 

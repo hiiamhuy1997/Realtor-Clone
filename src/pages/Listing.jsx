246b87f5240050d004c13ad7599b8c64b033cd05
Quang Huy <hiiamhuy160997@gmail.com>
2023-06-02 11:07:56 +0700

    Listing page completed

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -5,13 +5,18 @@ import { db } from "../firebase";
 import { Swiper, SwiperSlide } from "swiper/react";
 import SwiperCore, { EffectFade, Autoplay, Navigation, Pagination } from "swiper";
 import "swiper/css/bundle";
-import { FaShare } from "react-icons/fa";
+import { FaShare, FaMapMarkedAlt, FaBed, FaBath, FaParking, FaChair } from "react-icons/fa";
+import { getAuth } from "firebase/auth";
+import Contact from "../components/Contact";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 
 const Listing = () => {
+  const auth = getAuth();
   const params = useParams();
   const [listing, setListing] = useState(null);
   const [loading, setLoading] = useState(true);
   const [linkPopUp, setLinkPopUp] = useState(false);
+  const [contactLandlord, setContactLandlord] = useState(false);
   SwiperCore.use([Autoplay, Navigation, Pagination]);
 
   useEffect(() => {
@@ -69,6 +74,81 @@ const Listing = () => {
           Link Copied
         </p>
       )}
+
+      <div className="flex flex-col md:flex-row max-w-6xl lg:mx-auto p-4 m-4 rounded-lg  shadow-lg bg-white lg:space-x-5">
+        <div className=" w-full">
+          <p className="text-2xl font-bold mb-3 text-blue-900">
+            {listing.name} - $
+            {listing.offer
+              ? listing.discountPrice.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+              : listing.regularPrice.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+            {listing.type === "rent" ? " / month" : ""}
+          </p>
+          <p className="flex items-center gap-2 mt-6 mb-3 font-semibold truncate">
+            <FaMapMarkedAlt className="text-green-700" />
+            {listing.address}
+          </p>
+          <div className="flex justify-start items-center space-x-4 w-[75%]">
+            <p className="bg-red-800 w-full max-w-[200px] rounded-md p-1 text-white text-center font-semibold shadow-md">
+              {listing.type === "rent" ? "Rent" : "Sell"}
+            </p>
+            <p className="bg-green-800 w-full max-w-[200px] rounded-md p-1 text-white text-center font-semibold shadow-md">
+              {listing.offer && <p>${+listing.regularPrice - +listing.discountPrice} discount</p>}
+            </p>
+          </div>
+          <p className="my-3">
+            <span className="font-semibold">Description - </span>
+            {listing.description}
+          </p>
+          <ul className="flex items-center space-x-4 lg:space-x-10 text-sm font-semibold">
+            <li className="flex items-center whitespace-nowrap gap-1">
+              <FaBed className="text-lg" />
+              {+listing.bedRooms > 1 ? `${listing.bedRooms} Beds` : "1 Bed"}
+            </li>
+            <li className="flex items-center whitespace-nowrap gap-1">
+              <FaBath className="text-lg" />
+              {+listing.bathRooms > 1 ? `${listing.bathRooms} Baths` : "1 Bath"}
+            </li>
+            <li className="flex items-center whitespace-nowrap gap-1">
+              <FaParking className="text-lg" />
+              {+listing.parking ? "Parking spot" : "No Parking"}
+            </li>
+            <li className="flex items-center whitespace-nowrap gap-1">
+              <FaChair className="text-lg" />
+              {+listing.furnish ? "Furnished" : "Not furnished"}
+            </li>
+          </ul>
+          {listing.userRef !== auth.currentUser?.uid && !contactLandlord && (
+            <div className="mt-6">
+              <button
+                onClick={() => setContactLandlord(true)}
+                className="px-7 py-3 bg-blue-600 text-white font-medium text-sm uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg w-full text-center transition duration-150 ease-in-out"
+              >
+                Contact Landlord
+              </button>
+            </div>
+          )}
+          {contactLandlord && <Contact userRef={listing.userRef} listing={listing} />}
+        </div>
+        <div className="w-full h-[200px] md:h-[260px] lg:h-[400px] z-10 overflow-x-hidden mt-6 lg:mt-0 md:ml-2">
+          <MapContainer
+            center={[listing.geoLocation.lat, listing.geoLocation.lng]}
+            zoom={13}
+            scrollWheelZoom={false}
+            style={{ height: "100%", width: "100%" }}
+          >
+            <TileLayer
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={[listing.geoLocation.lat, listing.geoLocation.lng]}>
+              <Popup>
+                A pretty CSS3 popup. <br /> Easily customizable.
+              </Popup>
+            </Marker>
+          </MapContainer>
+        </div>
+      </div>
     </main>
   );
 };
